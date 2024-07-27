7f393ad0935e38b0861a795aefe9c569966d5aa2
mth_sousa <matheus.soares@jacto.com.br>
2024-07-27 12:34:42 -0300

    [NEW] Start screen text and music starts on play

--- game_gui.py
+++ game_gui.py
@@ -1,12 +1,22 @@
 import sys
 import pygame
 from pygame.locals import *
+from pygame import mixer 
 
 from  camera import Camera
 from player import Player
 
 # Init pygame engine
-pygame.init()
+pygame.init() # screen
+mixer.init() # sound
+pygame.font.init() # text
+game_font = pygame.font.SysFont('Comic Sans MS', 30)
+
+# Loading the song 
+mixer.music.load("Los Del Rio - Macarena (Bayside Boys Remix).mp3") 
+  
+# Setting the volume 
+mixer.music.set_volume(0.7) 
 
 # Predefined some colors
 BLUE  = (0, 0, 255)
@@ -29,9 +39,11 @@ pygame.display.set_caption("Let's Dance!")
 game_camera = Camera(); 
 running = False
 
-def game_start():
-    
-    running = True
+# Initial game screen
+start_text = game_font.render("Let's Dance!\n Click to start!", False, (0, 0, 0))
+
+# Create player instance
+player = Player()
 
 #Game loop begins
 while True:
@@ -45,7 +57,13 @@ while True:
         # Game starts on click
         if (not running)and (event.type == pygame.MOUSEBUTTONDOWN):
             print('Started game!')
+            # Start playing the song 
+            mixer.music.play()
             running = True
+    
+    # Game start text
+    if not running:
+        display.blit(start_text, (0,0))
 
     # Updates camera frame 
     success = game_camera.update()
@@ -58,8 +76,8 @@ while True:
 
     # Display current image
     display.blit(game_camera.get_frame(), (0, 0))
-
-   
+    
+    # Mostrar coreografia na tela e dar pontos
 
     # Update the camera screen
     pygame.display.update()
