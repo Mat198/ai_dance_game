d429e07679e6eee5500b11b068bcebe0889bffd2
mth_sousa <matheus.soares@jacto.com.br>
2024-07-27 12:34:05 -0300

    [CHG] Changing keypoints to a full dictionary
    Make it easy to save as json

--- keypoints.py
+++ keypoints.py
@@ -57,28 +57,32 @@ def extract_keypoint(keypoint: np.ndarray) -> dict[str, Point]:
     
     # Dictionary with the keypoint for meaningfull access.
     keypoints = {
-        "nose": Point(nose_x, nose_y), 
-        "left_eye": Point(left_eye_x, left_eye_y),
-        "right_eye": Point(right_eye_x, right_eye_y),
-        "left_ear": Point(left_ear_x, left_ear_y),
-        "right_ear": Point(right_ear_x, right_ear_y),
-        "left_shoulder": Point(left_shoulder_x, left_shoulder_y),
-        "right_shoulder": Point(right_shoulder_x, right_shoulder_y),
-        "left_elbow": Point(left_elbow_x, left_elbow_y),
-        "right_elbow": Point(right_elbow_x, right_elbow_y),
-        "left_wrist": Point(left_wrist_x, left_wrist_y),
-        "right_wrist": Point(right_wrist_x, right_wrist_y),
-        "left_hip": Point(left_hip_x, left_hip_y),
-        "right_hip": Point(right_hip_x,right_hip_y),
-        "left_knee": Point(left_knee_x,left_knee_y),
-        "right_knee": Point(right_knee_x,right_knee_y),
-        "left_ankle": Point(left_ankle_x, left_ankle_y),
-        "right_ankle": Point(right_ankle_x, right_ankle_y)
+        "nose": {"x":int(nose_x), "y": int(nose_y)}, 
+        "left_eye": {"x":int(left_eye_x), "y": int(left_eye_y)},
+        "right_eye": {"x":int(right_eye_x), "y": int(right_eye_y)},
+        "left_ear": {"x":int(left_ear_x), "y": int(left_ear_y)},
+        "right_ear": {"x":int(right_ear_x), "y": int(right_ear_y)},
+        "left_shoulder": {"x":int(left_shoulder_x), "y": int(left_shoulder_y)},
+        "right_shoulder": {"x":int(right_shoulder_x), "y": int(right_shoulder_y)},
+        "left_elbow": {"x":int(left_elbow_x), "y": int(left_elbow_y)},
+        "right_elbow": {"x":int(right_elbow_x), "y": int(right_elbow_y)},
+        "left_wrist": {"x":int(left_wrist_x), "y": int(left_wrist_y)},
+        "right_wrist": {"x":int(right_wrist_x), "y": int(right_wrist_y)},
+        "left_hip": {"x":int(left_hip_x), "y": int(left_hip_y)},
+        "right_hip": {"x":int(right_hip_x), "y":int(right_hip_y)},
+        "left_knee": {"x":int(left_knee_x), "y":int(left_knee_y)},
+        "right_knee": {"x":int(right_knee_x), "y":int(right_knee_y)},
+        "left_ankle": {"x":int(left_ankle_x), "y": int(left_ankle_y)},
+        "right_ankle": {"x":int(right_ankle_x), "y": int(right_ankle_y)}
     }
     return keypoints
 
 
 def get_xy_keypoint(results: Results) -> dict[str, Point]:
+    if len(results.keypoints.xy.cpu().numpy()) == 0:
+        return None
     result_keypoint = results.keypoints.xy.cpu().numpy()[0]
+    if len(result_keypoint) == 0:
+        return None
     keypoint_data = extract_keypoint(result_keypoint)
     return keypoint_data
